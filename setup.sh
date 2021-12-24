b5d314aced3b5de06d1c99d8025f585485a06704
jackson-waschura <jwaschur@calpoly.edu>
2021-12-24 09:37:11 -0800

    Added object_detection install to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,32 @@ echo "-------------------------"
 mkdir env
 python3 -m venv env
 source env/bin/activate
-pip install wheel
-pip install -r requirements.txt
-pip install -e .
+python -m pip install --upgrade pip
+python -m pip install wheel
+
+sudo apt-get install -y protobuf-compiler
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error installing protobuf-compiler (protoc)"
+    echo "Terminating early."
+    exit $retVal
+fi
+
+# ============= TF Object Detection API =============
+pushd models/research
+
+# Compile protos.
+protoc object_detection/protos/*.proto --python_out=.
+# Install TensorFlow Object Detection API.
+cp object_detection/packages/tf2/setup.py .
+python -m pip install .
+
+popd
+
+# ============= bioacoustic-detection =============
+python -m pip install -r requirements.txt
+python -m pip install -e .
 
 echo "-------------------------"
 echo "Finished running setup.sh"
 echo "-------------------------"
-
